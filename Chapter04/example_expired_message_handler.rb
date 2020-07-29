e036aafc78c63d4ef0f293e2b64aa340ecd96628
utkarsha-Packt <utkarshak@packt.com>
2020-07-29 17:10:43 +0530

    Update example_expired_message_handler.rb

--- Chapter04/example_expired_message_handler.rb
+++ Chapter04/example_expired_message_handler.rb
@@ -2,33 +2,32 @@
 require "bunny"
 
 #	2. Read RABBITMQ_URI from ENV
-connection = Bunny.new ENV['RABBITMQ_URI']
+connection = Bunny.new ENV["RABBITMQ_URI"]
 
 #	3. Communication session
 connection.start
 channel = connection.create_channel
 
 # 4. Declare a queue
-queue1 = channel.queue('taxi-inbox.1', durable: true,
-
-                       arguments: {'x-message-ttl'=> 604800000, 'x-dead-letter-exchange'=> 'taxi-dlx'})
+queue1 = channel.queue("taxi-inbox.1", durable: true,
+                       arguments: {"x-message-ttl"=> 604800000, "x-dead-letter-exchange"=> "taxi-dlx"})
 
 # 5. Declare a queue
-queue2 = channel.queue('taxi-inbox.2', durable: true,
-                       arguments: {'x-message-ttl'=> 604800000, 'x-dead-letter-exchange'=> 'taxi-dlx'})
+queue2 = channel.queue("taxi-inbox.2", durable: true,
+                       arguments: {"x-message-ttl"=> 604800000, "x-dead-letter-exchange"=> "taxi-dlx"})
 
 # 6. Declare a fanout exchange
-exchange = channel.fanout('taxi-fanout')
+exchange = channel.fanout("taxi-fanout")
 
 #	7. Bind queues to exchanges
 queue1.bind(exchange, routing_key: "")
 queue2.bind(exchange, routing_key: "")
 
 #	8. Declare a dead letter queue
-taxi_dlq = channel.queue('taxi-dlq', durable: true)
+taxi_dlq = channel.queue("taxi-dlq", durable: true)
 
 #	9. Declare dead letter fanout exchange
-dlx_exchange = channel.fanout('taxi-dlx')
+dlx_exchange = channel.fanout("taxi-dlx")
 
 #	10. Bind taxi-dlx to taxi-dlq
 taxi_dlq.bind(dlx_exchange, routing_key: "")

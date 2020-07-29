c35b1669cdc06c95885a06efe7bcab6ebf3314e4
utkarsha-Packt <utkarshak@packt.com>
2020-07-29 17:38:12 +0530

    Update mirror_queues.sh

--- Chapter06/mirror_queues.sh
+++ Chapter06/mirror_queues.sh
@@ -1,2 +1,2 @@
-sudo rabbitmqctl clear_policy -p cc-prod-vhost Q_TTL_DLX
-sudo rabbitmqctl set_policy -p cc-prod-vhost HA_Q_TTL_DLX
+rabbitmqctl clear_policy -p cc-prod-vhost Q_TTL_DLX
+rabbitmqctl set_policy -p cc-prod-vhost HA_Q_TTL_DLX

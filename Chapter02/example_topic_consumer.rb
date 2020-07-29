e1072d0c8829b479d761317bc0e8f9d51d9e0e14
utkarsha-Packt <utkarshak@packt.com>
2020-07-29 16:42:26 +0530

    Update example_topic_consumer.rb

--- Chapter02/example_topic_consumer.rb
+++ Chapter02/example_topic_consumer.rb
@@ -2,28 +2,35 @@
 require "bunny"
 
 #	Read RABBITMQ_URI from ENV
-connection = Bunny.new ENV['RABBITMQ_URI']
+connection = Bunny.new ENV["RABBITMQ_URI"]
 
 #	Start a communication session with RabbitMQ
 connection.start
 channel = connection.create_channel
+
+#	Method for the processing
+def process_order(info)
+  puts "Handling taxi order"
+  puts info
+  sleep 5.0
+  puts "Processing done"
+end
 
 def taxi_topic_subscribe(channel, taxi, type)
   # Declare a queue for a given taxi
   queue = channel.queue(taxi, durable: true)
 
   # Declare a topic exchange
-  exchange = channel.topic('taxi-topic', durable: true, auto_delete: true)
+  exchange = channel.topic("taxi-topic", durable: true, auto_delete: true)
 
   #	Bind the queue to the exchange
   queue.bind(exchange, routing_key: type)
 
   #	Bind the queue to the exchange to make sure the taxi will get any order
-  queue.bind(exchange, routing_key: 'taxi')
+  queue.bind(exchange, routing_key: "taxi")
 
   #	Subscribe from the queue
-  queue.subscribe(block:true,manual_ack: false) do |delivery_info, properties, payload|
-
+  queue.subscribe(block: true, manual_ack: false) do |delivery_info, properties, payload|
     process_order(payload)
   end
 end

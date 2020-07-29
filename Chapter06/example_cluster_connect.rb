68acc9f12a924ed7a5e5a25ba69eb390acd4a944
utkarsha-Packt <utkarshak@packt.com>
2020-07-29 17:37:40 +0530

    Update example_cluster_connect.rb

--- Chapter06/example_cluster_connect.rb
+++ Chapter06/example_cluster_connect.rb
@@ -1,8 +1,17 @@
 require "bunny"
 
+RABBITMQ_HOST1 = ENV["RABBITMQ_HOST1"]
+RABBITMQ_HOST2 = ENV["RABBITMQ_HOST2"]
+USERNAME = ENV["RABBITMQ_USER"]
+PASSWORD = ENV["RABBITMQ_PASSWORD"]
+VHOST = ENV["RABBITMQ_VHOST"]
+
 begin
+  # try to connect to one of the two hosts where the queue is mirrored or federated
+  # alternatively, uses addresses: [addr1, addr2,...] with ipv4 host and port in place
+  # of hosts
   connection = Bunny.new(
-      hosts: ['rmq-prod-01', 'rmq-prod-02'])
+      hosts: [RABBITMQ_HOST1, RABBITMQ_HOST2], username: USERNAME, password: PASSWORD)
   connection.start
 rescue Bunny::TCPConnectionFailed => e
   puts "Connection to server failed"

df33258988de41ea41af1026dcdecb7df7e8c3f3
utkarsha-Packt <utkarshak@packt.com>
2020-07-29 16:37:10 +0530

    Update create_queue_and_exchange.rb

--- Chapter02/create_queue_and_exchange.rb
+++ Chapter02/create_queue_and_exchange.rb
@@ -1,10 +1,8 @@
+#	1. Require client library
 require "bunny"
 
-#	1. Require client library
-require 'bunny'
-
 #	2. Read RABBITMQ_URI from ENV
-connection = Bunny.new ENV['RABBITMQ_URI']
+connection = Bunny.new ENV["RABBITMQ_URI"]
 
 #	3. Start a communication session with RabbitMQ
 connection.start
@@ -13,11 +11,11 @@ channel = connection.create_channel
 def on_start(channel)
 
   #	4.	Declare a queue for a given taxi
-  queue = channel.queue('taxi.1', durable: true)
+  queue = channel.queue("taxi.1", durable: true)
   #	5.	Declare a direct exchange, taxi-direct
-  exchange = channel.direct('taxi-direct', durable: true, auto_delete: true)
+  exchange = channel.direct("taxi-direct", durable: true, auto_delete: true)
   #	6. Bind the queue to the exchange
-  queue.bind(exchange, routing_key: 'taxi.1')
+  queue.bind(exchange, routing_key: "taxi.1")
   #	7. Return the exchange
   exchange
 end

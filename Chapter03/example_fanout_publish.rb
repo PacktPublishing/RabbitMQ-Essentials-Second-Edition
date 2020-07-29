bd2e334045768abe3d630a5eec68f46d298dd44c
utkarsha-Packt <utkarshak@packt.com>
2020-07-29 17:01:32 +0530

    Update example_fanout_publish.rb

--- Chapter03/example_fanout_publish.rb
+++ Chapter03/example_fanout_publish.rb
@@ -2,18 +2,18 @@
 require "bunny"
 
 #	2. Read RABBITMQ_URI from ENV
-connection = Bunny.new ENV['RABBITMQ_URI']
+connection = Bunny.new ENV["RABBITMQ_URI"]
 
 #	3. Communication session with RabbitMQ
 connection.start
 channel = connection.create_channel
 
 #	4. Declare queues for taxis
-queue1 = channel.queue('taxi-inbox.1', durable: true)
-queue2 = channel.queue('taxi-inbox.2', durable: true)
+queue1 = channel.queue("taxi-inbox.1", durable: true)
+queue2 = channel.queue("taxi-inbox.2", durable: true)
 
 # 5. Declare a fanout exchange
-exchange = channel.fanout('taxi-fanout')
+exchange = channel.fanout("taxi-fanout")
 
 #	6. Bind the queue
 queue1.bind(exchange, routing_key: "")

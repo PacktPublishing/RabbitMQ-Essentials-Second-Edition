90373ae134715d1d4dd529bb10ed8b4b27de3037
utkarsha-Packt <utkarshak@packt.com>
2020-07-29 17:10:57 +0530

    Update queue_patch.sh

--- Chapter04/queue_patch.sh
+++ Chapter04/queue_patch.sh
@@ -1,1 +1,1 @@
-sudo rabbitmqctl set_policy -p cc-dev-vhost Q_TTL_DLX "taxi\.\d+" '{"message-ttl":604800000, "dead-letter-exchange":"taxi-dlx"}' --apply-to queues
+rabbitmqctl set_policy -p cc-dev-vhost Q_TTL_DLX "taxi\.\d+" "{"message-ttl":604800000, "dead-letter-exchange":"taxi-dlx"}" --apply-to queues
